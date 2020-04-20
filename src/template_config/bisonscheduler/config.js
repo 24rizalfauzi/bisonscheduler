cbfe2fa1517006a3b18d38ac643f83e63455edc1
rzlfauzi <xsis.rizalf@xl.co.id>
2020-04-20 17:25:26 +0700

    '.'

--- src/template_config/bisonscheduler/config.js
+++ src/template_config/bisonscheduler/config.js
@@ -17,9 +17,9 @@ if (env==='dev') {
 	var smtpAuthUser	= '24rizalfauzi@gmail.com'
 	var smtpAuthPass	= '"84d=CEg_`S(@SwN'
 	//GIT
-	var dirbisonmiddleware		= 'E:/EP/BISONDOCS/github/prod/bison_middleware' // tempat folder middleware di server
-	var dirbisonscheduler		= 'E:/EP/BISONDOCS/github/prod/bison_scheduler' // tempat folder scheduler di server
-	var dirbisonweb				= 'E:/xampp/htdocs/bisonweb' //bisonweb yang ada di htdocs
+	var dirbisonmiddleware		= 'E:\\EP\\BISONDOCS\\github\\prod\\bison_middleware' // tempat folder middleware di server
+	var dirbisonscheduler		= 'E:\\EP\\BISONDOCS\\github\\prod\\bison_scheduler' // tempat folder scheduler di server
+	var dirbisonweb				= 'E:\\xampp\\htdocs\\bisonweb' //bisonweb yang ada di htdocs
 	var gitpullbisonmiddleware	= 'git pull https://24rizalfauzi:*****r1zalf4Uzi*****@github.com/24rizalfauzi/bisonmiddleware.git' //buat pull otomatis
 	var gitpullbisonweb			= 'git pull https://24rizalfauzi:*****r1zalf4Uzi*****@github.com/24rizalfauzi/bisonweb.git' //buat pull otomatis
 	var gitpullbisonscheduler	= 'git pull https://24rizalfauzi:*****r1zalf4Uzi*****@github.com/24rizalfauzi/bisonscheduler.git' //buat pull otomatis
